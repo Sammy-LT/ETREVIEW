4daf7516e9884632701c8ebafc4f767f12364509
Sammy-LT <samuel1296lt@gmail.com>
2025-07-17 18:33:32 +0300

    fix: add better-auth secret and config

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,14 +1,14 @@
-import{ betterAuth } from "better-auth";
+import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "@/lib/prisma";
+
 export const auth = betterAuth({
-    database: prismaAdapter(prisma,{
-        provider: "postgresql",
-    }),
-     emailAndPassword: {    
-        enabled: true,
-        minPasswordLength:6,
-    } ,
-
-
+  secret: process.env.BETTER_AUTH_SECRET!, // ✅ MUST include this
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+    minPasswordLength: 6,
+  },
 });
